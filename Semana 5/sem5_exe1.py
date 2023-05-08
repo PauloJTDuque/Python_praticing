4f7a5309ffe285d3df134f550bdd60ecfba4cf43
Paulo Duque <paulo.duque@terra.com.br>
2023-05-08 08:11:50 -0300

    Commit 080523

---
+++ Semana 5/sem5_exe1.py
@@ -0,0 +1,21 @@
+def bubble_sort(v):
+    for i in range(len(v)-1):
+        if (v[j])>v[j+1]:
+            v[j],v[j+1] = v[j+1],v[j]
+            print(v)
+
+v=(20,19,18,17,16,15):
+bubble_sort(v)
+
+def insertion_sort(v):
+    for i in range(1, len(v)):
+        x = v[i]
+        j=  i-1
+print(v)
+
+        while J>= 0 and x < (v[j])>v[j]:
+            v[j+1] = v[j]
+            j -= 1
+            v[j+1] = x
+
+print(v)
