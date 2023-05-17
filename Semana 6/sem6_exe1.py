d295431923515dddc565c2e4c369419d808a9113
Paulo Duque <paulo.duque@terra.com.br>
2023-05-17 08:23:30 -0300

    Commit 170523A

--- Semana 6/sem6_exe1.py
+++ Semana 6/sem6_exe1.py
@@ -1,9 +1,22 @@
 from urllib.request import urlopen
+from html.parser import HTMLParser
+
+class MyParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for attr in attrs:
+                if attr[0] == 'href':
+                    print(attr[1])
 
 def getSource(url):
     response = urlopen(url)
     html = response.read()
     return html.decode()
 
-html = getSource("http://www.uol.com.br")
-print(html)
+html = getSource("https://www.uol.com.br")
+#print(html)
+
+parser = MyParser()
+parser.feed(html)
+
+
