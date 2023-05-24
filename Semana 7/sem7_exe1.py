03b04b73237acc313f475c84b2ff40d07d3c558f
Paulo Duque <paulo.duque@terra.com.br>
2023-05-24 10:33:32 -0300

    Commit 240523A

--- Semana 7/sem7_exe1.py
+++ Semana 7/sem7_exe1.py
@@ -1,8 +1,9 @@
-from tkinter import Tk, Label
+from tkinter import Tk, Label, PhotoImage
 
 
 root = Tk()
-hello = Label(master=root, text='Olá classe!')
+photo = PhotoImage(file='computer.gif').subsample(2)
+hello = Label(master=root, image=photo, width=300, height=180)
 ## pack() faz o enpacotamento
 hello.pack()
 root.mainloop()
